4592742661b1e390d98c0002a9b10a924faa810b
Nestor <161639528+NeNest0r@users.noreply.github.com>
2026-02-13 20:29:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ================= CONFIG =================
 const firebaseConfig = {
-  apiKey: "AIzaSyA0K4w6sY9Drc2hO4m6biJdBrcGFvqTC2M",
-  authDomain: "chat-clicker.firebaseapp.com",
-  projectId: "chat-clicker",
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -1131,3 +1131,4 @@ function clearOsu() {
     )
     .forEach((el) => el.remove());
 }
+
